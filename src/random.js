81ed8c2452eb348961261d19d956ee5fcb53640e
merestrickland <mere.strickland@gmail.com>
2019-09-17 23:33:15 -0400

    random looping

--- src/random.js
+++ src/random.js
@@ -5,14 +5,19 @@
 // }
 
 function setup(){
-    let canvas = createCanvas(500, 500)
-    canvas.center()
+    let canvas = createCanvas(windowWidth, windowHeight)
+    frameRate(2)
+    // canvas.center()
 }
 
 function draw(){
     let x = 10
     while(x < width){
         ellipse(x, height/2, 20, 20)
-        x+=60
+        x+=10
+    }
+
+    for(let i=0; i<=height; i+=10) {
+        ellipse(random(10, width), i, 20, 20)
     }
 }
